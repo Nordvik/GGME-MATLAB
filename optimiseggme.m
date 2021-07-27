a2ba41028d950ca55223e9631c7453acb7b055b4
apj4x <86233025+apj4x@users.noreply.github.com>
2021-07-27 21:51:10 +0100

    Bug fixes in optimiseggme

--- optimiseggme.m
+++ optimiseggme.m
@@ -3,6 +3,10 @@ function [bestCs, bestCMs, bestWs] = optimiseggme(N, adjList,instancesToProcess,
 %then further processes them to see if they can be improved and outputs the
 %new best instance
 
+
+%format tree name for printing
+tree = strjoin(string(adjList));
+
 %set parameters if unset
 if nargin < 4
     instancesToOutput = 5;
@@ -12,14 +16,15 @@ if nargin < 3
     instancesToProcess = 20;
 end
 
+
 %load in data
-filenames = dir(strcat('OutputMatrices\',string(N),'modes\',adjList,'\*.mat');
+filenames = dir(strcat('OutputMatrices\',string(N),'modes\',tree,'\*.mat'));
 CMarray=[];Warray=[];Clist=[];
 
 for i=1:length(filenames)
-    CMarray = cat(3, CMarray, load(strcat('OutputMatrices\',string(N),'modes\',adjList,'\',filenames(i),'.mat'),garray));
-    Warray = cat(3, Warray, load(strcat('OutputMatrices\',string(N),'modes\',adjList,'\',filenames(i),'.mat'),warray));
-    Clist = [Clist, load(strcat('OutputMatrices\',string(N),'modes\',adjList,'\',filenames(i),'.mat'),carray)];
+    CMarray = cat(3, CMarray, load(strcat('OutputMatrices\',string(N),'modes\',tree,'\',filenames(i).name),"garray").garray);
+    Warray = cat(3, Warray, load(strcat('OutputMatrices\',string(N),'modes\',tree,'\',filenames(i).name),"warray").warray);
+    Clist = [Clist, load(strcat('OutputMatrices\',string(N),'modes\',tree,'\',filenames(i).name),"carray").carray];
 end
 
 %find most optimal instances
@@ -34,7 +39,7 @@ end
 improved = repelem(false, instancesToProcess); %log if an improvement is made
 blindfold = getBlindness(N, adjList); %get blindfold
 for i=1:instancesToProcess
-    [c, W, CM] = findggme(CMarray(i), N, true, 30, blindfold, adjList);
+    [c, W, CM] = findggme(CMarray(:,:,i), N, true, 30, blindfold, tree, 30);
     if c < Clist(i)
         improved(i) = true;
         %add new improved instance to arrays
@@ -57,7 +62,18 @@ for i=1:instancesToOutput
     tempClist(outputindexes(i)) = 1;
 end
 
-save(strcat('OutputMatrices\',string(N),'modes\',adjList,'\optimalCases.mat',bestCMs, bestWs, bestCs);
+    
+save(strcat('OutputMatrices\',string(N),'modes\',tree,'\optimal\optimalCases.mat'),"bestCMs", "bestWs", "bestCs");
 
+%output indexes of instances that are improved
+"These instances were improved by further processing:"
+count = 0;
+for i=improved
+    count = count + 1;
+    if i 
+        count
+    end
+end
+        
 end
 
